a1e300fd171e7043bc3a304133d1770b34345b7b
filip barszczewski <fbarszczewski@outlook.com>
2024-07-23 14:30:13 +0200

    added email & emailAttachment entities

---
+++ SimpleMailboxClient/Entities/Email.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleMailboxClient.Entities;
+
+public class Email
+{
+    public required string MessageId { get; set; }
+    public string Subject { get; set; }
+    public string Author { get; set; }
+    public string From { get; set; }
+    public string[] To { get; set; }
+    public string[] Cc { get; set; }
+    public string TextBody { get; set; }
+    public DateTime Date { get; set; }
+    //Folder on mailbox where  message is stored
+    public string Folder { get; set; }
+}
---
+++ SimpleMailboxClient/Entities/EmailAttachment.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleMailboxClient.Entities
+{
+    public class EmailAttachment
+    {
+
+        public string MessageId { get; set; }
+        public string FilePath { get; set; }
+        public string FileName { get; set; }
+        public string FileExtension { get; set; }
+        public long Size { get; set; }
+    }
+}
